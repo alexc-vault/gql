ba85fab28e120a0ac21e348a362acc03e7048a62
Alex 🙆🏻‍♂️ <alex@vaultplatform.com>
2022-07-28 11:43:58 +0100

    feat: Add UUID Scalar

--- packages/utils/src/utils/log.ts
+++ packages/utils/src/utils/log.ts
@@ -1,6 +1,6 @@
 export type Logger = (message: string, ...rest: any) => void;
 
-export function createLogger(id: string, userId?: number) {
+export function createLogger(id: string, userId?: string) {
   return (message: string, ...rest: any) => {
     const userPart = userId !== undefined ? userId : 'unknown';
     const restPart = rest.length ? '\n' + JSON.stringify(rest) : '';
